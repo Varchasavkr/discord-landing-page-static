a756f5cc75c263c9366cd87485027c71d1881272
varchasavkr <varchasav25@gmail.com>
2023-04-25 09:52:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontFamily:{
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"]
+      }
+
+    },
+
+
+
+
+  },
+  plugins: [],
+
+
+}
